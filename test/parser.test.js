c25b8ebe8a7f1b5b0c27d997c0f028523a1add6b
CSaltx <cobytyler7@gmail.com>
2024-02-15 16:08:29 -0800

    fixed a few grammar issues and fixed the parser tests, add more tests before friday

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,19 +1,25 @@
-import assert from "node:assert/strict"
-import parse from "../src/parser.js"
+import assert from "node:assert/strict";
+import parse from "../src/parser.js";
 
 const syntaxChecks = [
   ["all numeric literal forms", "print(8 * 89.123);"],
   ["complex expressions", "print(83 * ((((-((((13 / 21)))))))) + 1 - 0);"],
   ["all unary operators", "print (-3); print (!false);"],
-  ["all binary operators", "print x && y || z * 1 / 2 ** 3 + 4 < 5;"],
-  ["all arithmetic operators", "let x = (!3) * 2 + 4 - (-7.3) * 8 ** 13 / 1;"],
-  ["all relational operators", "let x = 1<(2<=(3==(4!=(5 >= (6>7)))));"],
-  ["all logical operators", "let x = true && false || (!false);"],
-  ["the conditional operator", "print x ? y : z;"],
+  ["all binary operators", "serve(x && y || z * 1 / 2 ** 3 + 4 < 5);"],
+  [
+    "all arithmetic operators",
+    "ingredient x := (!3) * 2 + 4 - (-7.3) * 8 ** 13 / 1;",
+  ],
+  [
+    "all relational operators",
+    "ingredient x := 1<(2<=(3==(4!=(5 >= (6>7)))));",
+  ],
+  ["all logical operators", "ingredient x := true && false || (!false);"],
+  ["the conditional operator", "serve(x ? y : z);"],
   ["end of program inside comment", "print(0); // yay"],
   ["comments with no text are ok", "print(1);//\nprint(0);//"],
   ["non-Latin letters in identifiers", "コンパイラ = 100;"],
-]
+];
 
 const syntaxErrors = [
   ["non-letter in an identifier", "ab😭c = 2", /Line 1, col 3/],
@@ -26,17 +32,17 @@ const syntaxErrors = [
   ["an illegal statement on line 2", "print(5);\nx * 5;", /Line 2, col 3/],
   ["a statement starting with a )", "print(5);\n) * 5", /Line 2, col 1/],
   ["an expression starting with a *", "x = * 71;", /Line 1, col 5/],
-]
+];
 
 describe("The parser", () => {
   for (const [scenario, source] of syntaxChecks) {
     it(`properly specifies ${scenario}`, () => {
-      assert(parse(source).succeeded())
-    })
+      assert(parse(source).succeeded());
+    });
   }
   for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
     it(`does not permit ${scenario}`, () => {
-      assert.throws(() => parse(source), errorMessagePattern)
-    })
+      assert.throws(() => parse(source), errorMessagePattern);
+    });
   }
-})
+});
