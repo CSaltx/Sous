9a99b0718671e40dc78d73c6c5e1f29ba475425f
Milla Penelope Markovic <mmarkov1@lion.lmu.edu>
2024-03-31 09:07:55 -0700

    Attempting to add responsiveness for mobile.  Needs to be tested.

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -5,4 +5,11 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  screens: {
+    'sm' : '640px',
+    'md' : '768',
+    'lg' : '1024px',
+    'xl' : '1280px',
+    '2xl' : '1536px'
+  }
 };
