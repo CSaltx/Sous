28baa13aa97e2712954cd7016d8fac447a1898b2
CSaltx <cobytyler7@gmail.com>
2024-04-02 14:15:46 -0700

    finished website with bios, responsive design

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'xs': {'max': '639px'},
-      'sm': {'min': '640px', 'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px', 'max': '1535px'},
-      '2xl': {'max': '1536px'}
-    }
+      //   xxs: { max: "389px", min: "0px" },
+      xs: { max: "639px", min: "0px" },
+      sm: { min: "640px", max: "767px" },
+      md: { min: "768px", max: "1023px" },
+      lg: { min: "1024px", max: "1279px" },
+      xl: { min: "1280px", max: "1535px" },
+      "2xl": { max: "1536px" },
+    },
   },
   plugins: [],
 };
