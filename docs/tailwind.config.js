7980caaa8988e860878dff2deeee7284738c9c87
CSaltx <cobytyler7@gmail.com>
2024-04-02 22:39:03 -0700

    attempt to fix height issue with examples

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       md: { min: "768px", max: "1023px" },
       lg: { min: "1024px", max: "1279px" },
       xl: { min: "1280px", max: "1535px" },
-      "2xl": { max: "1536px" },
+      xxl: { max: "1536px" },
     },
   },
   plugins: [],
